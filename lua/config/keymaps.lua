13cc5ee1a7f7cff34a0ffeac2834c1c0d8898934
Emad Mamaghani <emad.mamaghani@nn.nl>
2024-08-29 13:10:35 +0200

    feat: copilot

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local cmp = require("cmp")
+
+cmp.setup({
+  -- Other configuration options...
+
+  mapping = {
+    -- Existing mappings...
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    -- Other mappings...
+  },
+
+  -- Other configuration options...
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
   {
-    "askfiy/visual_studio_code",
+    "morhetz/gruvbox",
     priority = 100,
     config = function()
-      vim.cmd([[colorscheme visual_studio_code]])
+      vim.cmd([[colorscheme gruvbox]])
     end,
   },
 }
